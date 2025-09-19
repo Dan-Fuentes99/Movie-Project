e845874174541776a629fc2680becaf8ace8a19c
Dan-Fuentes99 <fuentes14@icloud.com>
2025-09-19 15:12:55 -0400

    update

--- src/App.js
+++ src/App.js
@@ -1,19 +1,17 @@
 import React from 'react';
 import './App.css';
-import { HashRouter, Routes, Route } from 'react-router-dom';
+import { Routes, Route } from 'react-router-dom';
 import Home from './Pages/Home/Home.jsx';
 import MovieDetails from './Pages/MovieDetails/MovieDetails.jsx';
 import Search from './Pages/Search/Search';
 
 function App() {
   return (
-    <HashRouter>
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/movie/:id" element={<MovieDetails />} />
         <Route path="/search" element={<Search />} />
       </Routes>
-    </HashRouter>
   );
 }
 
